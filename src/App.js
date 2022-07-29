4c50eeb5ac992acb99a5849df672946e3f068f89
AMD <michojekunle1@gmail.com>
2022-07-29 17:45:28 +0100

    Movie -Project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+
+const API_URL = 'https://www.omdbapi.com?apikey=1bd49d0b'
 
 const App = () => {
+    
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+    }
+    
+    useEffect(() => {
+
+    }, []);
+
   return (
-    <div>App</div>
+    <div>
+
+    </div>
   )
 }
 
